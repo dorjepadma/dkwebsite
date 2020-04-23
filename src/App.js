78f678c286e741b1e07632bc9c91eef2b9421520
dorjepadma <dorjepadma@gmail.com>
2020-04-23 11:47:54 -0700

    home page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Dorje Kirsten',
+      headerLinks: [
+        { title: 'Home', path: '/'},
+        { title: 'Bio', path: '/Bio'},
+        { title: 'Contact', path: '/Contact'},
+        { title: 'Heart Astrology', path: '/www.heartastrology.com'},
+        { title: 'Serpent Paintings', path: '/www.serpentpainting.com'},
+        { title: 'Good Teas', path: '/www.planet-teas.com'}
+      ],
+      home: {
+        title: 'Transforms the world',
+        subTitle: 'Working for a better tomorrow',
+        text: 'A few of the projects I have worked on'
+      },
+      bio: {
+        title: 'A taste of my story'
+      },
+      contact: {
+        title: 'Get in touch'
+      },
+    }
+  }
+  render() {
+    return (
+      <div>Hello dear fans!</div>
+    );
+  }
 }
 
 export default App;
