fab47109f89cbf331f823c162296ccdc20707184
dorjepadma <dorjepadma@gmail.com>
2020-04-24 11:28:48 -0700

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Container from 'react-bootstrap/container';
+import Row from 'react-bootstrap/row';
+import Col from 'react-bootstrap/col';
+
+function Footer() {
+  return(
+    <footer className='mt-5'>
+      <Container fluid={true}>
+        <Row className="border-top justify-content p-3">
+        <Col className='p-0' md={3} sm={12} >
+          Dorje Kirsten
+        </Col>
+        <Col className='p-0 d-flex justify-content-end' md={3}>
+          Site built by Dorje Kirsten
+        </Col>
+        </Row>
+      </Container>
+    </footer>
+
+  )
+}
+export default Footer;
