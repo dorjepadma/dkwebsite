b32d5086ab861c3bb9a42624b664d939f73ee0a2
dorjepadma <dorjepadma@gmail.com>
2020-04-23 22:47:40 -0700

    nav bar in

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-import NavbarBrand from 'react-bootstrap/NavbarBrand';
+import Nav from 'react-bootstrap/Nav';
 import './App.css';
 
 class App extends React.Component {
@@ -14,9 +14,6 @@ class App extends React.Component {
         { title: 'Home', path: '/'},
         { title: 'Bio', path: '/Bio'},
         { title: 'Contact', path: '/Contact'},
-        { title: 'Heart Astrology', path: '/www.heartastrology.com'},
-        { title: 'Serpent Paintings', path: '/www.serpentpainting.com'},
-        { title: 'Good Teas', path: '/www.planet-teas.com'}
       ],
       home: {
         title: 'Transforms the world',
@@ -36,10 +33,19 @@ class App extends React.Component {
       <Router>
         <Container className='p-0' fluid={true}>
 
-          <Navbar>
-            <Navbar.Brand>Dorje Kirsten</Navbar.Brand>
-          </Navbar>
+          <Navbar className='border-bottom' bg='transparent' expand='lg'>
 
+            <Navbar.Brand>Dorje Kirsten</Navbar.Brand>
+
+            <Navbar.Toggle className='border-0' aria-controls='navbar-toggle' />
+            <Navbar.Collapse id='navbar-toggle'>
+              <Nav className='ml-auto'>
+                <Link className='nav-link' to="/">Home</Link>
+                <Link className='nav-link' to="/Bio">Bio</Link>
+                <Link className='nav-link' to="/Contact">Contact</Link>
+              </Nav>
+          </Navbar.Collapse>
+          </Navbar>
         </Container>
       </Router>
     );
