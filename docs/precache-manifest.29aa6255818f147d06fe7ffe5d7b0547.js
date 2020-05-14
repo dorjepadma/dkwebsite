b7fe5ed9e537bf1a517b31768f14845e3da098f5
dorjepadma <dorjepadma@gmail.com>
2020-05-13 17:47:44 -0700

    added build

---
+++ docs/precache-manifest.29aa6255818f147d06fe7ffe5d7b0547.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f6f56ddfffc286b68b0c12a35adfc25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e130555b378244027ff6",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "45728b1d5f548d5b6939",
+    "url": "/static/css/main.7e392c57.chunk.css"
+  },
+  {
+    "revision": "e130555b378244027ff6",
+    "url": "/static/js/2.e7c1214f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e7c1214f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45728b1d5f548d5b6939",
+    "url": "/static/js/main.d2d065f8.chunk.js"
+  },
+  {
+    "revision": "cb4f6c3e42fd57bda448",
+    "url": "/static/js/runtime-main.443b9e64.js"
+  },
+  {
+    "revision": "700bfd7528c0ddffd9d714ddc859fae0",
+    "url": "/static/media/Khendo-Khani.700bfd75.jpg"
+  },
+  {
+    "revision": "2f16dfa46342fb9d335deebb1523f4f9",
+    "url": "/static/media/coffee.2f16dfa4.jpg"
+  },
+  {
+    "revision": "c338d4e162bd8a7e8fa862fbdc7047b6",
+    "url": "/static/media/cosmo.c338d4e1.jpg"
+  },
+  {
+    "revision": "ddee79a6147151ffc57d0f8cf80c5d0d",
+    "url": "/static/media/poegram.ddee79a6.png"
+  }
+]);
