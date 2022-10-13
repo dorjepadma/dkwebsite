f0281ef243eca9e6d020d53a1c4705e9927fe3db
dorjepadma <dorjepadma@gmail.com>
2022-10-13 12:24:34 -0700

    made changes to bio

--- src/pages/BioPage.js
+++ src/pages/BioPage.js
@@ -7,7 +7,7 @@ function BioPage(props) {
   <div>
     <Avatar title={props.title} />
     <Content>
-      <p>Thank you for stopping by.  I am a full stack engineer who is skilled in React, Javascript, NodeJS, Redux, Firebase, and SEO. This is folded into a poet, astrologer, tea master, and contemplative.  </p>
+      <p>Thank you for stopping by.  I am a full stack engineer who is skilled in React, Javascript, NodeJS, Redux, Firebase, Bootstrap, UI/UX design and SEO. This is folded into the life of a poet, astrologer, tea master, and contemplative.  </p>
 
       <p>I spent my twenties and thirties living at a Nyingma Buddhist retreat center, Chagdud Gonpa Rigdzin Ling. I left to raise a family and practice astrology. On the way I founded Planet Teas. After filling myself with plenty of tea, I became a software engineer.
       </p>
